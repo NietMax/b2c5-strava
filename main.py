9284d2501f4682357bdfeacf7239519fc33d782c
nietmax <maxgoldsmits@gmail.com>
2024-04-09 00:06:32 +0200

    added the table function

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
+
 # General imports for calling the Strava API
 import requests
-
+import pandas as pd
 
 def get_strava_data():
-    url = "https://www.strava.com/api/v3/athlete"
+    url = "https://www.strava.com/api/v3/activities/"
     headers = {"Authorization": "Bearer f2852e3c857f6aa06aa4771c09510da7d29ab91e"}
 
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        return response.json()
         print("Success! Code" + str(response.status_code))
+        return response.json()
     else:
         return None
 
-
 data = get_strava_data()
 if data is not None:
-    print(data)
+    # Convert the data to a pandas DataFrame and print it
+    df = pd.DataFrame([data])
+    print(df)
 else:
     print("Failed to get data from Strava API | Or no data available")
