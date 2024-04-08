8bc32b7dae7c1d82c4130170b26cba7873c56be3
Justin Schots <justinschots13@gmail.com>
2024-04-09 00:04:55 +0200

    dotenv and error handling

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 # General imports for calling the Strava API
 import requests
+import os
+from dotenv import load_dotenv
+
+# Load the environment variables
+load_dotenv()
 
 
 def get_strava_data():
     url = "https://www.strava.com/api/v3/athlete"
-    headers = {"Authorization": "Bearer f2852e3c857f6aa06aa4771c09510da7d29ab91e"}
+    bearer_token = os.getenv("BearerToken")
+    headers = {"Authorization": f"Bearer ${bearer_token}"}
 
     response = requests.get(url, headers=headers)
 
     if response.status_code == 200:
-        return response.json()
         print("Success! Code" + str(response.status_code))
+        return response.json()
     else:
+        print("Failed to get data from Strava API | Or no data available")
         return None
 
 
