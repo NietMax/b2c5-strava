491e1aef2a37ceec155c79f7194e92ff6d05ea5e
Justin Schots <justinschots13@gmail.com>
2024-04-18 13:57:07 +0200

    added upgrade pip

--- run.ps1
+++ run.ps1
@@ -4,8 +4,11 @@ python -m venv env
 # Activate the virtual environment
 .\env\Scripts\activate
 
+# Upgrade pip
+python -m pip install --upgrade pip
+
 # Install the required modules
-pip install -r requirements.txt
+pip install -r requirements.txt.
 
 # Run your script
 python main.py
--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
 # Create a virtual environment
 python -m venv env
 
 # Activate the virtual environment
 source env/bin/activate
+
+# Upgrade pip
+python -m pip install --upgrade pip
 
 # Install the required modules
 pip install -r requirements.txt
