3868dabf0da0d1511d24c62f6ac13de6853d1a15
Justin Schots <justinschots13@gmail.com>
2024-04-18 12:02:14 +0200

    recommit because of repo crash

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
-# General imports for calling the Strava API
-import requests
 import os
-from dotenv import load_dotenv
+import shutil
+from datetime import datetime
+from pathlib import Path
+import pandas as pd
 
-# Load the environment variables
-load_dotenv()
+from src.api_methods import get_methods
+from src.api_methods import authorize
+from src.data_preprocessing import main as data_prep
 
+# used to f.e set the limit of fetched activities (default - 30)
+ACTIVITIES_PER_PAGE = 200
+# current page number with activities
+PAGE_NUMBER = 1
 
-def get_strava_data():
-    url = "https://www.strava.com/api/v3/athlete"
-    bearer_token = os.getenv("BEARER_TOKEN")
-    headers = {"Authorization": f"Bearer {bearer_token}"}
+GET_ALL_ACTIVITIES_PARAMS = {
+    'per_page': ACTIVITIES_PER_PAGE,
+    'page': PAGE_NUMBER
+}
 
-    response = requests.get(url, headers=headers)
+def main():
+    # Clean the data directory
+    folder = 'data'
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
-    if response.status_code == 200:
-        print("Success! Code" + str(response.status_code))
-        return response.json()
-    else:
-        print("Failed to get data from Strava API | Or no data available")
-        return None
+    token:str = authorize.get_acces_token()
+    data:dict = get_methods.access_activity_data(token, params=GET_ALL_ACTIVITIES_PARAMS)
+    df = data_prep.preprocess_data(data)
+    # Convert the dictionary to a DataFrame
+    df = pd.DataFrame(df)
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    df.to_csv(Path('data', f'my_activity_data={timestamp}.csv'), sep=',', index=False)
+    # Transpose the DataFrame
+    df = df.transpose()
+    # Print the DataFrame as a table
+    print(df)
 
-
-data = get_strava_data()
-if data is not None:
-    print(data)
-else:
-    print("No data received from Strava API")
+if __name__ == '__main__':
+    main()
